d15086b7b66501eef42e05e477b88189e47abf01
Chaitanya Narhare <chetannarhare111@gmail.com>
2024-03-02 10:26:53 +0530

    added code to draw nodes recursively with canvas

--- drawBinaryTree.js
+++ drawBinaryTree.js
@@ -1,5 +1,5 @@
 import { BinaryTreeNode } from './BinaryTreeNode.js'
-import {getRequiredHeightandWidth} from './treeUtils.js'
+import {DEFAULT_CONFIG, connectEdges, drawNode, getRequiredHeightandWidth} from './treeUtils.js'
 
 const canvas = document.querySelector('canvas')
 
@@ -27,7 +27,7 @@ function drawBinaryTree (root, canvasElement) {
     xStart,
     xEnd,
   }  
-  recursivelyDrawNodes(root, canvasElement, 0, horizontalConfig)
+  recursivelyDrawNodes(root, canvasElement, 0.5, horizontalConfig)
 }
   
   //Algo: 
@@ -36,16 +36,57 @@ function drawBinaryTree (root, canvasElement) {
   // 3) Recursively draw left and right nodes
   // 4) connect edges of root with left and right 
 function recursivelyDrawNodes (root, canvasElement, currentLevel, horizontalConfig) {
+  const { xStart, xEnd } = horizontalConfig
+  // Node coord.
+  const xPos = (xStart + xEnd) / 2
+  const yPos = currentLevel * DEFAULT_CONFIG.nodeHeightSpacing
   
+  drawNode(root.value, canvasElement, xPos, yPos)
+
+  if(root.left !== null){
+    const horizontalConfig = { xStart , xEnd: xPos }
+    const yLeftEnd = ((currentLevel + 1) * DEFAULT_CONFIG.nodeHeightSpacing) - DEFAULT_CONFIG.radius 
+    const xLeftEnd = (xStart + xPos) / 2
+    recursivelyDrawNodes(root.left, canvasElement, currentLevel + 1, horizontalConfig)
+    connectEdges(canvasElement, {xStart: xPos, xEnd: xLeftEnd}, { yStart: yPos + DEFAULT_CONFIG.radius, yEnd: yLeftEnd  })
+    
+  }
+  
+  if(root.right !== null){
+    const horizontalConfig = { xStart: xPos, xEnd }
+    recursivelyDrawNodes(root.right, canvasElement, currentLevel + 1, horizontalConfig)
+    const xRightEnd = (xPos + xEnd) / 2
+    const yLeftEnd = ((currentLevel + 1) * DEFAULT_CONFIG.nodeHeightSpacing) - DEFAULT_CONFIG.radius 
+    connectEdges(canvasElement, {xStart: xPos, xEnd: xRightEnd}, { yStart: yPos + DEFAULT_CONFIG.radius, yEnd: yLeftEnd  })
+  }
 }
 
 const root = new BinaryTreeNode(10)
 
-const node1 = new BinaryTreeNode(20)
-root.setLeft(node1)
+const node2 = new BinaryTreeNode(20)
+root.setLeft(node2)
 
-const node2 = new BinaryTreeNode(30)
-root.setRight(node2)
+const node3 = new BinaryTreeNode(30)
+root.setRight(node3)
+
+const node4  = new BinaryTreeNode(40)
+node2.setLeft(node4) 
+
+const node5  = new BinaryTreeNode(null)
+node2.setRight(node5) 
+
+const node6 = new BinaryTreeNode(30)
+node3.setLeft(node6)
+
+const node7 = new BinaryTreeNode(30)
+node3.setRight(node7)
+
+const node8 = new BinaryTreeNode(90)
+node4.setLeft(node8)
+
+const node9 = new BinaryTreeNode(80)
+node4.setRight(node9)
+
 
 drawBinaryTree(root, canvas)
 console.log(root)
--- treeUtils.js
+++ treeUtils.js
@@ -2,7 +2,7 @@ export const DEFAULT_CONFIG = {
   radius: 20,
   nodeWidthSpacing: 25, 
   nodeHeightSpacing: 100,
-  fontSize: 10
+  fontSize: 14
 }
 
 export function getRequiredHeightandWidth (root) {
@@ -16,4 +16,48 @@ export function getRequiredHeightandWidth (root) {
     requiredCanvasHeight,
     requiredCanvasWidth
   }
+}
+
+export function drawNode( value, canvasElement, x, y){
+  const context = canvasElement.getContext('2d')
+  
+  //draw circle
+  context.beginPath()
+  context.arc(x, y, DEFAULT_CONFIG.radius, 0, 2 * Math.PI, false)
+  context.fillStyle = 'lightsalmon'
+  context.fill()
+  
+  //draw border
+  context.beginPath()
+  context.arc(x, y, DEFAULT_CONFIG.radius, 0, 2 * Math.PI, false)
+  context.strokeStyle = 'brown'
+  context.stroke()
+  
+  // fill text
+  context.beginPath()
+  context.font = `${DEFAULT_CONFIG.fontSize}px serif`;
+  context.fillStyle = "black";
+  context.textAlign = "center";
+  context.fillText(value, x, y + DEFAULT_CONFIG.fontSize / 2);
+}
+
+export function connectEdges(canvasElement, xCoordinates, yCoordinates) {
+  console.log({canvasElement, xCoordinates, yCoordinates})
+  const { xStart, xEnd} = xCoordinates
+  const { yStart, yEnd } = yCoordinates
+  
+  const xHalf = (xStart + xEnd) / 2;
+  const yHalf = (yStart + yEnd ) / 2
+  
+  const cpoint1 = { x: xHalf, y: yHalf}
+  const cpoint2 = { x: xEnd, y: yHalf}
+  // Draw curve
+  const context = canvasElement.getContext('2d')
+  context.beginPath()
+  context.strokeStyle = 'brown'
+  context.moveTo(xStart, yStart)
+  context.bezierCurveTo(cpoint1.x, cpoint1.y, cpoint2.x, cpoint2.y, xEnd, yEnd)
+  
+  // context.lineTo(xEnd, yEnd)
+  context.stroke()
 }
